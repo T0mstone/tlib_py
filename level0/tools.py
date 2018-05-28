0da5ed7eeb23e42aab0d3f06920839eb8dfd6c16
T0mstone <realt0mstone@gmail.com>
2018-05-28 20:12:34 +0200

    code optimizing

--- level0/tools.py
+++ level0/tools.py
@@ -1,12 +1,10 @@
-import re
-
-
-def selectFromArray(array, condition):
+def selectFromArray(array, condition_func):
     res = []
     for e in array:
-        if condition(e):
+        if condition_func(e):
             res.append(e)
     return res
+
 
 def getListItem(list, index, default):
     if len(list) > index:
@@ -14,15 +12,23 @@ def getListItem(list, index, default):
     else:
         return default
 
-done = []
+
+once_done = []
+
+
 def doOnce(id, func):
-    if not done.__contains__(id):
-        done.append(id)
+    if not once_done.__contains__(id):
+        once_done.append(id)
         func()
 
-def average(l):
-    if type(l) in (tuple, list):
-        return sum(l) / len(l)
+
+def average(_iter):
+    try:
+        _list = list(_iter)
+    except TypeError:
+        # I know that this can occur
+        raise
+    return sum(_list) / len(_list)
 
 
 def map_value(val, start1, stop1, start2, stop2):
