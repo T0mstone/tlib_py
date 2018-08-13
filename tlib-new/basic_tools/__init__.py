71b7b6cfffeea871aa48203b0d033ea10a34c23f
T0mstone <realt0mstone@gmail.com>
2018-08-13 15:48:05 +0200

    cleaned up some no longer used files

--- tlib-new/basic_tools/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import collection_tools
-from . import number_tools
-from . import string_tools
-from . import other_tools
--- tlib-new/basic_tools/other_tools.py
+++
@@ -1,9 +0,0 @@
-_done = []
-_done_id_counter = 0
-def doOnce(func, *args, id=None):
-    if id is None:
-        id = _done_id_counter
-        _done_id_counter += 1
-    if id not in _done:
-        _done.append(id)
-        func(*args)
